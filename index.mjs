c1cabcf921945c39df508a342d2f8aa68c571f4e
Faysal Ahmed <faysalahmed.dev@gmail.com>
2024-06-11 20:37:49 +0600

    fix: date format
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 const app = express();
 
@@ -9,6 +12,19 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'hello' });
 });
+
+function formatDate(date) {
+    // Get individual components of the date
+    var year = date.getFullYear();
+    var month = ('0' + (date.getMonth() + 1)).slice(-2); // Months are zero-based
+    var day = ('0' + date.getDate()).slice(-2);
+    var hours = ('0' + date.getHours()).slice(-2);
+    var minutes = ('0' + date.getMinutes()).slice(-2);
+    var seconds = ('0' + date.getSeconds()).slice(-2);
+
+    // Construct the formatted date string
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
 
 console.log('user name: ', process.env.TERRAPAY_USERNAME);
 app.get('/get-test-mfs', (req, res) => {
