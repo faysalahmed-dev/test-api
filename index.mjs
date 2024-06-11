f19e82a756067b8c6d43c6d495134dfe75d1fd22
Faysal Ahmed <faysalahmed.dev@gmail.com>
2024-06-11 20:25:53 +0600

    test wallet list

--- index.mjs
+++ index.mjs
@@ -9,6 +9,25 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'hello' });
 });
 
+console.log('user name: ', process.env.TERRAPAY_USERNAME);
+app.post('/get-test-mfs', (req, res) => {
+    axios
+        .get('https://uat-connect.terrapay.com:21211/eig/getwalletlist/BD', {
+            headers: {
+                'X-USERNAME': process.env.TERRAPAY_USERNAME,
+                'X-PASSWORD': process.env.TERRAPAY_PASSWORD,
+                'X-ORIGINCOUNTRY': 'AU',
+                'X-DATE': formatDate(new Date()),
+            },
+        })
+        .then(result => {
+            res.json({ data: result.data });
+        })
+        .catch(err => {
+            res.json({ error: err });
+        });
+});
+
 app.listen(5555, () => {
     console.log('server is running on the port: 5555');
 });
